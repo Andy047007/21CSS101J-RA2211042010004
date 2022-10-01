09efb57fa53cc2bcdd792637f19b3d52a77572e5
Neel Delivala <113873544+Andy047007@users.noreply.github.com>
2022-10-01 11:54:17 +0530

    Add files via upload

---
+++ Q)2.18.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+//To find and print the larger integer from user input
+
+int main(){
+
+int x, y;
+
+printf("Enter value of x:");
+scanf("%d", &x);
+
+printf("Enter value of y:");
+scanf("%d", &y);
+
+if(x>y)
+    printf("%d is larger\n", x);
+else if(x<y)
+        printf("%d is larger\n" , y);
+else
+printf("These numbers are equal");
+
+}
---
+++ Q)2.19.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+//To input 3 different integers and find their sum, average, product, smallest and largest number
+
+int main(){
+
+int x, y, z;
+printf("Input three values:");
+scanf("%d", &x);
+scanf("%d", &y);
+scanf("%d", &z);
+
+printf("Sum is:%d\n", x+y+z);
+printf("Average is:%d\n", (x+y+z)/3);
+printf("Product is:%d\n", x*y*z);
+
+//Using if find smallest
+
+if(x<y)
+    if(x<z)
+        printf("Smallest is:%d\n", x);
+if(y<x)
+    if(y<z)
+        printf("Smallest is:%d\n", y);
+if(z<x)
+    if(z<y)
+        printf("Smallest is:%d\n", z);
+
+//Using if find largest
+
+if(x>y)
+    if(x>z)
+        printf("Largest is:%d\n", x);
+if(y>x)
+    if(y>z)
+        printf("Largest is:%d\n", y);
+if(z>x)
+    if(z>y)
+        printf("Largest is:%d\n", z);
+
+if(x==y==z)
+            printf("All are equal");
+
+
+
+
+
+}
