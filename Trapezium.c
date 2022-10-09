e1764e884eae221b190b8590a500c8947f341b65
Neel Delivala <113873544+Andy047007@users.noreply.github.com>
2022-10-09 22:50:08 +0530

    Add files via upload

---
+++ Trapezium.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+
+int base1;
+int base2;
+int height;
+float area;
+
+printf("Enter value of base1 of Trapezium:");
+scanf("%d", &base1);
+
+printf("Enter value of base2 of Trapezium:");
+scanf("%d", &base2);
+
+printf("Enter value of height of Trapezium:");
+scanf("%d", &height);
+
+area=(base1+base2)*height/2;
+
+printf("The Area of Trapezium is:%0.2f", area);
+
+int side1, side2;
+
+printf("Enter value of side1 and side2:");
+scanf("%d,%d", &side1,&side2);
+
+printf("The Perimeter of Trapezium is:%d", side1+side2+base1+base2);
+}
