9421cdb20bf33cf1536ffef112aea7ee65255ff9
Neel Delivala <113873544+Andy047007@users.noreply.github.com>
2022-10-09 16:29:38 +0530

    Add files via upload

---
+++ ID Card.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+printf("\t\tName: NEEL DELIVALA\n");
+printf("\t\tProgramme: B.Tech.(CS&BS)\n");
+printf("\t\tRegister No.: RA2211042010004\n");
+printf("\t\tValid From : Jun-2022 To May-2025\n");
+
+
+}
