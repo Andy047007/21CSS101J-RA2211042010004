440cc19399626226e810bcb1432c582e3800c10a
Neel Delivala <113873544+Andy047007@users.noreply.github.com>
2022-10-09 22:59:31 +0530

    Add files via upload

---
+++ Rhombus.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+int diagonal1;
+int diagonal2;
+int side;
+
+printf("Enter value of diagonal1:");
+scanf("%d", &diagonal1);
+
+printf("Enter value of diagonal2:");
+scanf("%d", &diagonal2);
+
+printf("Enter value of side:");
+scanf("%d", &side);
+
+float area;
+area=(diagonal1*diagonal2)/2;
+
+printf("Area of Rhombus is:%0.2f\n", area);
+
+printf("Perimeter of Rhombus is:%d\n", 4*side);
+
+
+}
