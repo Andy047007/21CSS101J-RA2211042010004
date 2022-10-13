d72efe4c3daa277e845020630984cba95761595d
Neel Delivala <113873544+Andy047007@users.noreply.github.com>
2022-10-13 19:49:07 +0530

    Update Multiplication Table.c

--- Multiplication Table.c
+++ Multiplication Table.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 
-int main()
-{
-    int i,a,n;
-    printf("Enter the Number whose Table you want to Print: \n");
-    scanf("%d",&n);
+int main(){
+printf("Registration NO.:RA2211042010004\n");
+int i,a,n;
+printf("Enter the Number whose Table you want to Print: \n");
+scanf("%d",&n);
 
-    for(i=1;i<=10;i++)
-    {
-        a=n*i;
-        printf("%d * %d = %d\n", n, i, n*i);
-    }
+for(i=1;i<=10;i++){
 
-    return 0;
+a=n*i;
+
+printf("%d * %d = %d\n", n, i, n*i);
+}
+
+return 0;
 }
