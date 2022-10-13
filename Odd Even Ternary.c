513afdfea7e7050423d2fc18ca7a40bc483714ba
Neel Delivala <113873544+Andy047007@users.noreply.github.com>
2022-10-13 19:50:00 +0530

    Update Odd Even Ternary.c

--- Odd Even Ternary.c
+++ Odd Even Ternary.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
-int main()
-{
-    int number;
-
-    printf("Enter a number to check for Even / Odd = ");
-    scanf("%d", &number);
-
-    (number%2==0)? printf("%d is Even",number):printf("%d is Odd",number);
-
-	return 0;
+int main(){
+printf("Registration NO.:RA2211042010004\n");
+   
+int number;
+printf("Enter a number to check for Even / Odd = ");
+scanf("%d", &number);
+(number%2==0)? printf("%d is Even",number):printf("%d is Odd",number);
+return 0;
 }
