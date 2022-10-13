d5458814a784ad442d2212bc3cc23ca95395c313
Neel Delivala <113873544+Andy047007@users.noreply.github.com>
2022-10-13 19:55:41 +0530

    Update Print first N natural Nos..c

--- Print first N natural Nos..c
+++ Print first N natural Nos..c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 int main(){
-    int n,i;
+printf("Registration NO.:RA2211042010004\n");
+    
+int n,i;
+printf("Enter a number to show natural numbers from 1 to N:");
+scanf("%d", &n);
 
-	printf("Enter a number to show natural numbers from 1 to N:");
-	scanf("%d", &n);
+printf("Numbers from 1 to %d are:\n",n);
 
-	printf("Numbers from 1 to %d are:\n",n);
-
-	for(i=1;i<=n;i++)
+for(i=1;i<=n;i++)
 	printf("%d\n", i);
 
     return 0;
