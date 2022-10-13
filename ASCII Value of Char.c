e02d558bca714b1326851cc3f1541ec5911d1171
Neel Delivala <113873544+Andy047007@users.noreply.github.com>
2022-10-13 19:36:29 +0530

    Update ASCII Value of Char.c

--- ASCII Value of Char.c
+++ ASCII Value of Char.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-printf("Registration NO.:RA2211042010004");
+printf("Registration NO.:RA2211042010004\n");
     char c;
     printf("Enter a character: ");
     scanf("%c", &c);
