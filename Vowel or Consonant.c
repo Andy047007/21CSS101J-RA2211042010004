8a8b0698eeb94ec050b99350fdafa92252a4c5b2
Neel Delivala <113873544+Andy047007@users.noreply.github.com>
2022-10-13 20:06:08 +0530

    Update Vowel or Consonant.c

--- Vowel or Consonant.c
+++ Vowel or Consonant.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main() {
+printf("Registration NO.:RA2211042010004\n");
     char c;
     int lowercase_vowel, uppercase_vowel;
-    printf("Enter an alphabet: ");
-    scanf("%c", &c);
 
-    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+printf("Enter an alphabet: ");
+scanf("%c", &c);
 
-    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+lowercase = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+uppercase = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
-    if (lowercase_vowel || uppercase_vowel)
-        printf("%c is a vowel.", c);
-    else
-        printf("%c is a consonant.", c);
-    return 0;
+if (lowercase_vowel || uppercase_vowel)
+    printf("%c is a vowel.", c);
+else
+    printf("%c is a consonant.", c);
 }
