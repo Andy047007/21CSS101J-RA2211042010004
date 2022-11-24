0ae28729af34a0e409d83bea7485f5f029a958e1
Neel Delivala <113873544+NeelDelivala@users.noreply.github.com>
2022-11-24 22:38:01 +0530

    Add files via upload

---
+++ Average grades.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ int main() {
+
+ int grade;
+ int total;
+ int average;
+
+ total = 0;
+
+while (counter<=10) {
+    printf( "%s", "Enter grade: " );
+    scanf( "%d", &grade );
+    total = total + grade;
+    counter=counter+1;
+    }
+}
+
